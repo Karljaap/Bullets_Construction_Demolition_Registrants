fb575f4d5fbc5b2c245d6645c05765ca31a678d3
Karljaap <a20185583@pucp.edu.pe>
2025-03-07 14:22:34 -0500

    Update Streamlit.py

--- Streamlit.py
+++ Streamlit.py
@@ -10,17 +10,11 @@ def load_data(path):
 DATA_PATH = "filtered_data_march_clean.csv"
 df = load_data(DATA_PATH)
 
-# Crear el mapa
+# Crear un mapa simple
 def create_map(data):
-    mapa = folium.Map(location=[data['latitude'].mean(), data['longitude'].mean()], zoom_start=12, tiles="OpenStreetMap")
-    for _, row in data.iterrows():
-        folium.Marker(
-            location=[row['latitude'], row['longitude']],
-            popup=row['account_name'],
-            tooltip=row['account_name']
-        ).add_to(mapa)
+    mapa = folium.Map(location=[data['latitude'].mean(), data['longitude'].mean()], zoom_start=10, tiles="Stamen Terrain")
     return mapa
 
 # Mostrar el mapa en Streamlit
-st.title("Mapa Interactivo")
+st.title("Mapa Simple")
 st_folium(create_map(df), width=700, height=500)
